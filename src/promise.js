56eef27749d264509f5ce299f37a76f02c724217
Bram Stein <stein@adobe.com>
2014-05-13 10:18:35 +0200

    Only export the Promise when it isn't available.

--- src/promise.js
+++ src/promise.js
@@ -230,10 +230,12 @@ goog.scope(function () {
   };
 });
 
-window['Promise'] = lang.Promise;
-window['Promise']['resolve'] = lang.Promise.resolve;
-window['Promise']['reject'] = lang.Promise.reject;
-window['Promise']['race'] = lang.Promise.race;
-window['Promise']['all'] = lang.Promise.all;
-window['Promise']['prototype']['then'] = lang.Promise.prototype.then;
-window['Promise']['prototype']['catch'] = lang.Promise.prototype.catch;
+if (!window['Promise']) {
+  window['Promise'] = lang.Promise;
+  window['Promise']['resolve'] = lang.Promise.resolve;
+  window['Promise']['reject'] = lang.Promise.reject;
+  window['Promise']['race'] = lang.Promise.race;
+  window['Promise']['all'] = lang.Promise.all;
+  window['Promise']['prototype']['then'] = lang.Promise.prototype.then;
+  window['Promise']['prototype']['catch'] = lang.Promise.prototype.catch;
+}
